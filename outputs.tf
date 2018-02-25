6dfb16580c4324419c47c398cc30aa1f0a5403df
Rupert Broad <rupert.broad@gmail.com>
2018-02-25 16:24:42 +0100

    add iam profile output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "alb_web_tg_arns" {
 output "alb_web_tg_names" {
   value = ["${module.load_balancers.alb_web_tg_names}"]
 }
+
+output "default_profile_name" {
+  value = "${module.iam.default_profile_name}"
+}
