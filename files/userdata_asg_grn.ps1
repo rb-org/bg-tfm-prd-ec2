ce079853ee0d214463a4150b32f1fcf4c13715bf
Rupert Broad <rupert.broad@gmail.com>
2018-02-25 15:33:03 +0100

    various

--- files/userdata_asg_grn.ps1
+++ files/userdata_asg_grn.ps1
@@ -1,4 +1,8 @@
 <powershell>
+$instanceid     = Invoke-RestMethod http://169.254.169.254/latest/meta-data/instance-id # this uses the EC2 instance ID as the node name
+$ami_id         = Invoke-RestMethod http://169.254.169.254/latest/meta-data/ami-id
+$tags           = Get-EC2Instance -Instance $instanceId | Select -ExpandProperty Instances | Select -ExpandProperty Tags
+$app_version    = $tags | ? Key -eq AppVersion | Select -ExpandProperty Value
 
 install-windowsfeature web-server, web-mgmt-console
 
@@ -7,8 +11,8 @@ $content = '
     <html>
     <body bgcolor="#009933">
     <font size=5>
-    <h1>Build ver - "20171221.58"</h1>
-    <h2>AMI id - "ami-c10b77b8"</h2>
+    <h1>Build ver - $app_version </h1>
+    <h2>AMI id - $ami_id </h2>
     </font>
     </body>
     </html>
