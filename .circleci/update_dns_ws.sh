aef6ffa7761ad8cdaf8f5d7a6f4955229733417f
Rupert Broad <rupert.broad@gmail.com>
2018-04-15 16:08:01 +0200

    separate asg and dns updates

---
+++ .circleci/update_dns_ws.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# We need to update the tfvars file with some environment variables from CircleCI
+echo "------------------------------------"
+echo "Updating secrets"
+echo -e  | tee -a env/"${WKSPC}".tfvars
+echo -e "acc_id = ${acc_id}" | tee -a env/"${WKSPC}".tfvars
+echo -e "allowed_ips = ${allowed_ips}" | tee -a env/"${WKSPC}".tfvars
+echo -e "cert_domain = ${cert_domain}" | tee -a env/"${WKSPC}".tfvars
+echo -e "zone_id = ${zone_id}" | tee -a env/"${WKSPC}".tfvars
+
+CCI_PROJECT=$CIRCLE_PROJECT_REPONAME
+CCI_USERNAME=$CIRCLE_PROJECT_USERNAME
+
+# AppVer and AMI Id vars have just been updated. We need to use these on the color that was previously secondary ASG
+# ws_running_color_dev tells us which color is currently in primary ASG
+
+ws_update_dns(){
+
+    echo "------------------------------------"
+    echo "Running ws_plan_dns"
+    if [[ $(echo "$ws_running_color_dev" |grep grn) = "grn" ]]; then
+        echo "Updating Green"
+
+        echo -e "bg-web-ws-ami_grn = {type = \"map\" eu-west-1 = \"${ws_ami_id_latest_dev}\"}" | tee -a env/"${WKSPC}".tfvars
+        echo -e "bg-web-ws-ami_blu = {type = \"map\" eu-west-1 = \"${ws_ami_id_running_dev}\"}" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "bg-web-ws-des_grn = 1" | tee -a env/"${WKSPC}".tfvars
+        echo -e "bg-web-ws-des_blu = 1" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "app_version_web_grn = \"${ws_app_ver_latest_dev}\""| tee -a env/"${WKSPC}".tfvars
+        echo -e "app_version_web_blu = \"${ws_app_ver_running_dev}\"" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "www_dns_weight_grn = 100" | tee -a env/"${WKSPC}".tfvars
+        echo -e "www_dns_weight_blu = 0" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "bg-web-ws = \"grn\"" | tee -a env/"${WKSPC}".tfvars
+
+    elif [[ $(echo "$ws_running_color_dev" |grep blu) = "blu" ]]; then
+        echo "Updating Blue"
+
+        echo -e "bg-web-ws-ami_blu = {type = \"map\" eu-west-1 = \"${ws_ami_id_latest_dev}\"}" | tee -a env/"${WKSPC}".tfvars
+        echo -e "bg-web-ws-ami_grn = {type = \"map\" eu-west-1 = \"${ws_ami_id_running_dev}\"}" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "bg-web-ws-des_blu = 1" | tee -a env/"${WKSPC}".tfvars
+        echo -e "bg-web-ws-des_grn = 1" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "app_version_web_blu = \"${ws_app_ver_latest_dev}\"" | tee -a env/"${WKSPC}".tfvars
+        echo -e "app_version_web_grn = \"${ws_app_ver_running_dev}\"" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "www_dns_weight_blu = 100" | tee -a env/"${WKSPC}".tfvars
+        echo -e "www_dns_weight_grn = 0" | tee -a env/"${WKSPC}".tfvars
+
+        echo -e "bg-web-ws = \"blu\"" | tee -a env/"${WKSPC}".tfvars   
+
+    else 
+        echo "Something went wrong"
+        echo "------------------------------------"
+        echo "${WKSPC}.tfvars"
+        echo
+        cat env/"${WKSPC}".tfvars
+        echo "------------------------------------"
+        echo "CircleCI Env Vars"
+        echo
+        echo "Latest AMI Id: ${ws_ami_id_latest_dev}"
+        echo "Running AMI Id: ${ws_ami_id_running_dev}"
+        echo "Latest App Ver: ${ws_app_ver_latest_dev}"
+        echo "Running App Ver: ${ws_app_ver_running_dev}"
+        echo "Running Color: "$ws_running_color_dev""
+        exit 1
+    fi
+    echo "------------------------------------"
+    cat env/"${WKSPC}".tfvars
+    echo "------------------------------------"
+
+}
+
+ws_update_dns
